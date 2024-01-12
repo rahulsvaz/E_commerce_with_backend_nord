da32fbd67eed89877859ca12eaed130d25b485f9
rahul <mrsvaz@icloud.com>
2024-01-13 00:06:26 +0530

    password hashed and adde mongoose .set in index.js

--- server/index.js
+++ server/index.js
@@ -4,21 +4,23 @@ const authRouter = require("./routes/auth");
 const app = express();
 const mongoose = require("mongoose");
 
+
 // middle ware
 app.use(express.json());
 app.use(authRouter);
-
+mongoose.set('strictQuery', true);
 //  we need to make connection to the database
 const DB =
   "mongodb+srv://mrsvaz:Readytorace799@cluster0.3mh6meu.mongodb.net/?retryWrites=true&w=majority";
 mongoose
-  .connect(DB) 
+  .connect(DB)
   .then(() => {
     console.log("connection successful");
   })
   .catch((e) => {
     console.log(e);
   });
-app.listen(PORT, () => {
+
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`connected to ${PORT}`);
 });
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,25 +1,33 @@
 const express = require("express");
 const User = require("../models/user");
 const authRouter = express.Router();
+const bcryptjs = require("bcryptjs");
 
 // creating post api
 
-authRouter.post("/api/signup", async (req,res)=>{
+authRouter.post("/api/signup", async (req, res) => {
+  try {
     // getting data from client
-    const {name,email,password} = req.body;
-    // post the data to database 
+    const { name, email, password } = req.body;
+    // post the data to database
 
-   const existingUser = await User.findOne({email});
-// status code 400 for client error we are not responsible for when user enters an existing user id so we use status code 400 its for client error
-   if(existingUser){
-   return res.status(400).json({msg:"user with same email already exists"});
-   }
-
-   let user = new User({
-    name,email,password
-   })
-   user = await user.save();
-   res.json(user); 
-
+    const existingUser = await User.findOne({ email });
+    // status code 400 for client error we are not responsible for when user enters an existing user id so we use status code 400 its for client error
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ msg: "user with same email already exists" });
+    }
+    const hashedPassword = await bcryptjs.hash(password, 8);
+    let user = new User({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    user = await user.save();
+    res.json(user);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
 });
 module.exports = authRouter;
