29db794cb79b95d5b1460aa4f4093162e4a47602
Rahul P <128817814+rahulsvaz@users.noreply.github.com>
2024-01-26 07:02:13 +0530

    user name bolded

--- lib/features/Home/view/widgets/below_app_bar.dart
+++ lib/features/Home/view/widgets/below_app_bar.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shopping_cart_with_node/common/global_variables/global_variables.dart';
@@ -11,18 +9,23 @@ class BelowAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final user = Provider.of<UserProvider>(context);
-    return  Container(
-      padding: EdgeInsets.only(left:10,right: 10,bottom: 10),
+    return Container(
+      padding: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
       width: MediaQuery.sizeOf(context).width,
-      decoration: BoxDecoration(
-        gradient: GlobalVariables.appBarGradient
-      ),
-      child:RichText(text: TextSpan(
-        text: 'Hello ',style: TextStyle(color: Colors.black,fontSize: 22),children: [TextSpan(
-        text: user.user.name,style: TextStyle(color: Colors.black,fontSize: 22),
-      )]
-
-      ),
+      decoration: const BoxDecoration(gradient: GlobalVariables.appBarGradient),
+      child: RichText(
+        text: TextSpan(
+            text: 'Hello ',
+            style: const TextStyle(color: Colors.black, fontSize: 22),
+            children: [
+              TextSpan(
+                text: user.user.name,
+                style: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 22,
+                    fontWeight: FontWeight.w600),
+              )
+            ]),
       ),
     );
   }
